f0ea8b20cee28f962a9f5d9754deb7db0b8925b1
OddCompost <richomas@hotmail.com>
2023-05-11 11:19:25 +0100

    added a header

--- oauthnoteapp/src/Header.tsx
+++ oauthnoteapp/src/Header.tsx
@@ -1,8 +1,6 @@
 
 function Header() {
 
-  //* Returns a list of todos as an unordered list of items with the todo name as the text of the list item
-  //* Everytime the input changes, the value of the input is set to the todoName varable
     return (
       <div className="header">
         <h1>Notes</h1>
--- oauthnoteapp/src/NoteSingle.tsx
+++ oauthnoteapp/src/NoteSingle.tsx
@@ -2,9 +2,7 @@
 function Note(props: {
   editTodo: any; id: React.Key | null | undefined; name: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; removeTodo: (arg0: any) => void; todo: { id: any; }; 
 }) {
-
-  //* Returns a list of todos as an unordered list of items with the todo name as the text of the list item
-  //* Everytime the input changes, the value of the input is set to the todoName varable
+  
     return (
             <li key={props.id}>
               {props.name}
